8aa95b07be0f40648188b497ce318462049c3e7e
Tom Dyas <tom.dyas@gmail.com>
2025-11-30 01:58:56 -0800

    add Sudoku puzzle generator (#6)
    Add logic to generate Sudoku puzzles using the DLX solver. First, a
    random Sudoku board is generated and then values are removed from the
    board up to the user's specified number of values to remove. `gg-cli`
    now has `sudoku generate` and `sudoku make-solved-board` to invoke the
    generation logic.

---
+++ src/sudoku/generate.rs
@@ -0,0 +1,174 @@
+use std::time::Instant;
+
+use rand::{Rng, seq::SliceRandom};
+
+use super::{SudokuBoard, SudokuDlxSolver, SudokuGraph};
+use crate::Board;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SudokuGenerateError {
+    /// No unique solution were found on this run of the generator. Try again.
+    NoUniqueSolutions,
+
+    // The number of clues to remove the board
+    InvalidNumberOfRequestedRemovals,
+
+    // The given Sudoku board is not fully filled.
+    IncompleteSuokduBoard,
+}
+
+pub fn generate_solved_sudoku_board<R: Rng>(
+    rng: &mut R,
+) -> Result<SudokuBoard, SudokuGenerateError> {
+    let mut solver = SudokuDlxSolver::new();
+
+    let mut board = Board::new(SudokuGraph::new().metadata);
+    let mut sudoku_board = SudokuBoard::empty();
+
+    // Shuffle the cell indexes so that the board is filled in a random order.
+    let indexes = {
+        let mut v = (0..SudokuGraph::NUM_CELLS).collect::<Vec<usize>>();
+        v.shuffle(rng);
+        v
+    };
+
+    for index in indexes {
+        // Get a list of possible values at the current index and shuffle for randomness.
+        let mut possible_values_for_cell = board.get_possible_values(index);
+        possible_values_for_cell.shuffle(rng);
+
+        // Try placing each possible value in the cell until there is at least one valid solution.
+        let mut placed_value = false;
+        for possible_value_for_cell in possible_values_for_cell {
+            board
+                .set_value(index, possible_value_for_cell)
+                .expect("value was already known to be possible");
+            sudoku_board
+                .set_value(index, Some(possible_value_for_cell))
+                .expect("value was already known to be possible");
+
+            let solutions = solver.solve_with_limit(&sudoku_board, Some(2));
+            if !solutions.is_empty() {
+                // If there is at least one possible solution with this value at this cell, then
+                // this placement was successful so break and continue with next cell.
+                placed_value = true;
+                break;
+            }
+
+            // If the placement failed because there are no solutions, then clear the cell
+            // and continue with trying possible values.
+            board
+                .reset_value(index)
+                .expect("restore cell value to original value");
+            sudoku_board.clear(index);
+        }
+
+        if !placed_value {
+            return Err(SudokuGenerateError::NoUniqueSolutions);
+        }
+    }
+
+    Ok(sudoku_board)
+}
+
+/// Generate a Sudoku puzzle by attempting to remove up to the specified number of clues
+/// from an existing valid solution. The caller should check the returned `SudokuBoard`
+/// to determine the number of given values actually removed from the board.
+pub fn remove_given_values_from_board<R: Rng>(
+    mut board: SudokuBoard,
+    max_num_values_to_remove: usize,
+    rng: &mut R,
+) -> Result<SudokuBoard, SudokuGenerateError> {
+    let mut solver = SudokuDlxSolver::new();
+
+    if max_num_values_to_remove == 0 || max_num_values_to_remove >= SudokuGraph::NUM_CELLS {
+        return Err(SudokuGenerateError::InvalidNumberOfRequestedRemovals);
+    }
+
+    log::debug!("Removing {max_num_values_to_remove} from board.");
+
+    // Shuffle the cell indexes so that the clues are removed in a random order.
+    let indexes = {
+        let mut v = (0..SudokuGraph::NUM_CELLS).collect::<Vec<usize>>();
+        v.shuffle(rng);
+        v
+    };
+
+    let mut num_values_removed = 0usize;
+    for index in indexes {
+        let prior_value = board
+            .value(index)
+            .ok_or(SudokuGenerateError::IncompleteSuokduBoard)?;
+        board.clear(index);
+
+        log::debug!("Trying removal of value {prior_value} in cell index {index}.");
+
+        let start_time = Instant::now();
+        let num_solutions = solver.solve_with_limit(&board, Some(2));
+        let uniqueness_check_duration = start_time.elapsed();
+
+        if num_solutions.len() == 1 {
+            // If the board still has a unique solution, even after removing this clue, then
+            // the placement was successful.
+            num_values_removed += 1;
+            log::debug!(
+                "Removal succeeded. num_values_removed={num_values_removed}. (Duration: {uniqueness_check_duration:?})"
+            );
+            if num_values_removed >= max_num_values_to_remove {
+                break;
+            }
+        } else {
+            // Removing this clue resulted in an unsolveable board or a board with multiple solutions.
+            // Put the value back and continue with trying other cells.
+            log::debug!(
+                "Removal failed due to no unique solution existing. (Duration: {uniqueness_check_duration:?})"
+            );
+            board
+                .set_value(index, Some(prior_value))
+                .expect("restoring original value");
+        }
+    }
+
+    Ok(board)
+}
+
+pub fn generate_sudoku_board_with_rng<R: Rng>(
+    max_num_values_to_remove: usize,
+    rng: &mut R,
+) -> Result<SudokuBoard, SudokuGenerateError> {
+    let solved_board = generate_solved_sudoku_board(rng)?;
+    let puzzle_board = remove_given_values_from_board(solved_board, max_num_values_to_remove, rng)?;
+    Ok(puzzle_board)
+}
+
+pub fn generate_sudoku_puzzle(
+    max_num_values_to_remove: usize,
+) -> Result<SudokuBoard, SudokuGenerateError> {
+    let mut rng = rand::rng();
+    generate_sudoku_board_with_rng(max_num_values_to_remove, &mut rng)
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::{SeedableRng, rngs::StdRng};
+
+    use super::*;
+
+    #[test]
+    fn generator_successfully_generates_a_unique_sudoku_puzzle() {
+        let mut rng = StdRng::seed_from_u64(0xDEADBEEF);
+
+        let num_clues_to_remove = 40;
+        let puzzle = generate_sudoku_board_with_rng(num_clues_to_remove, &mut rng)
+            .expect("generator should produce a valid puzzle");
+
+        assert_eq!(
+            puzzle.given_indices().count(),
+            SudokuGraph::NUM_CELLS - num_clues_to_remove
+        );
+
+        let mut solver = SudokuDlxSolver::new();
+        let solutions = solver.solve_with_limit(&puzzle, Some(2));
+        assert_eq!(solutions.len(), 1, "generated puzzle must be unique");
+    }
+}
